34973630c62c2ee5bb8b6946986e422fce2e6fa8
Elizabeth Needham <EliNeedham@outlook.com>
2020-08-29 13:44:40 -0500

    built out the atm ap except for the loop to the main menu

---
+++ CSF1Homework/ATM.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class ATM
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Welcome to the ATM");
+            Console.Title = "<============ ATM ==========>";
+
+            string accountNbr = "12345";
+           
+            int attempt = 0;
+            bool isCorrect = false;
+
+            while (attempt <=4 && isCorrect == false)
+            { 
+                Console.WriteLine("Please enter your 5 digit account number: ");
+                string userInput = Console.ReadLine();
+
+                attempt++;
+
+                if(userInput == accountNbr)
+                {
+                    isCorrect = true;
+                }
+
+            }
+
+            string pin = "1234";
+
+            int pinAttempt = 0;
+            bool isPin = false;
+
+            while (pinAttempt <= 4 && isPin == false)
+            {
+                Console.WriteLine("Please enter your 4 digit pin number: ");
+                string pinInput = Console.ReadLine();
+
+                pinAttempt++;
+
+                if (pinInput == pin)
+                {
+                    isPin = true;
+                }
+            }
+
+            //Once the user has successfully given their account and pin numbers, prompt the user with a menu and ask them if they want to do a deposit, a withdrawal, or exit the application. 
+            Console.WriteLine("\n\nPelase choose from the following options.\nWould you like to \n1)Deposit \n2)Withdrawal \nExit");
+
+            string userOption = Console.ReadLine().ToUpper();
+            switch (userOption)
+            {
+                case "1":
+                case "DEPOSIT":
+                case "D":
+                    Console.WriteLine("Please enter the amount you would like to deposit.");
+                    string deposit = Console.ReadLine();
+                    decimal depositA = decimal.Parse(deposit);
+                    Console.WriteLine($"{depositA:c} has been deposited into account # 12345.");
+                    Console.WriteLine("Would you like to do another deposit?");
+
+                    break;
+                case "2":
+                case "W":
+                case "WITHDRAWAL":
+                    Console.WriteLine("Please enter the amount you would like to deposit.");
+                    string withdrawal = Console.ReadLine();
+                    decimal withdrawalA = decimal.Parse(withdrawal);
+                    Console.WriteLine($"{withdrawalA:c} has been withdrawn from account # 12345.");
+                    Console.WriteLine("Would you like to do another withdrawal?");
+                    break;
+                case "3":
+                case "X":
+                case "EXIT":
+                case "E":
+                    Console.WriteLine("Thank you for visiting!");
+                    break;
+            }
+
+
+
+        }//end Main ()
+    }//end Class
+}//end Namespace
